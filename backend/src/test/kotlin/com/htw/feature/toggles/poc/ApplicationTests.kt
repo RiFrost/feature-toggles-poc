1a1b1e6af11be96066122f81c92d38aa19af49cc
RiFrost <ri.frost@web.de>
2023-01-20 16:33:15 +0100

    setup backend

---
+++ backend/src/test/kotlin/com/htw/feature/toggles/poc/ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.htw.feature.toggles.poc
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
