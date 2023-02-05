8f708802c6f4a09c0cceeb406cac8800822dc15f
RiFrost <ri.frost@web.de>
2023-02-05 15:41:58 +0100

    update Frontend Test description

--- frontend/src/components/__tests__/FeatureToggleView.test.js
+++ frontend/src/components/__tests__/FeatureToggleView.test.js
@@ -17,7 +17,7 @@ describe("FeatureToggleView with toggle off", () => {
     SHOW_UI_BUTTON: { enabled: false, name: "SHOW_UI_BUTTON" },
   };
 
-  it("doesn't render button when toggle 2 is off", () => {
+  it("doesn't render button when SHOW_UI_BUTTON is toggled off", () => {
     expect(wrapper.find(".toggle2-button").isVisible()).toBeFalsy();
   });
 });
@@ -33,7 +33,7 @@ describe("FeatureToggleView with toggle on", () => {
 
   store.toggles = { SHOW_UI_BUTTON: { enabled: true, name: "SHOW_UI_BUTTON" } };
 
-  it("renders button when toggle 2 is on", async () => {
+  it("renders button when SHOW_UI_BUTTON is toggled on", async () => {
     expect(wrapper.find(".toggle2-button").isVisible()).toBeTruthy();
   });
 });
